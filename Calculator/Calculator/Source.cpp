5b7dacfb84f42f81ecab989f1ebf66c9a803f750
Thomas Arsanious <tomyarsanious@gmail.com>
2026-05-07 21:51:27 +0300

    Basic Calculator Completed

--- Calculator/Calculator/Source.cpp
+++ Calculator/Calculator/Source.cpp
@@ -4,5 +4,28 @@ using namespace std;
 
 int main() {
 	int num1, num2;
-
+	char Operation;
+	cout << "Welcome to the Basic Calculator " << endl;
+	cout << "Enter expression (e.g. 22 + 13):	";
+	cin >> num1 >> Operation >> num2;
+	switch (Operation) {
+	case '+':
+		cout << "Result = " << num1 + num2;
+		break;
+	case '-':
+		cout << "Result = " << num1 - num2;
+		break;
+	case '*':
+		cout << "Result = " << num1 * num2;
+		break;
+	case '/':
+		if (num2 == 0)
+			cout << "Error Cannot Divide by zero!! " << endl;
+		else {
+			cout << "Result = " << num1 / num2;
+			break;
+		}
+	default:
+		cout << "Invalid Operator!";
+	}
 }
